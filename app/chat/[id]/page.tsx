a65a4ccdb42439c42831428f509e0224fadfe469
Anchit Sinha <anchit1909@gmail.com>
2023-03-31 11:26:05 +0530

    implemented sending chat data to firebase

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,5 +1,22 @@
-function page() {
-  return <div>page</div>;
+import Chat from "@/components/Chat";
+import ChatInput from "@/components/ChatInput";
+
+type Props = {
+  params: {
+    id: string;
+  };
+};
+
+//We get these props by default from route.
+function ChatPage({ params: { id } }: Props) {
+  return (
+    <div className="flex h-screen flex-col overflow-hidden">
+      {/* Chat */}
+      <Chat chatId={id} />
+      {/* Chat Input */}
+      <ChatInput chatId={id} />
+    </div>
+  );
 }
 
-export default page;
+export default ChatPage;
---
+++ components/ChatInput.tsx
@@ -0,0 +1,96 @@
+"use client";
+
+import { db } from "@/firebase";
+import { PaperAirplaneIcon } from "@heroicons/react/24/solid";
+import { addDoc, collection, serverTimestamp } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { FormEvent, useState } from "react";
+
+type Props = {
+  chatId: string;
+};
+
+function ChatInput({ chatId }: Props) {
+  const [prompt, setPrompt] = useState("");
+  const { data: session } = useSession();
+
+  // useSwr to get the model
+  const model = "text-davinci-003";
+
+  const sendMessage = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    if (!prompt) return;
+
+    const input = prompt.trim();
+    setPrompt("");
+
+    const message: Message = {
+      text: input,
+      createdAt: serverTimestamp(),
+      user: {
+        _id: session?.user?.email!,
+        name: session?.user?.name!,
+        avatar:
+          session?.user?.image! ||
+          `https://ui-avatars.com/api/?name=${session?.user?.name}`,
+      },
+    };
+
+    await addDoc(
+      collection(
+        db,
+        "users",
+        session?.user?.email!,
+        "chats",
+        chatId,
+        "messages"
+      ),
+      message
+    );
+    //Toast notification to say Loading
+
+    await fetch("/api/askQuestion", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify({
+        prompt: input,
+        chatId,
+        model,
+        session,
+      }),
+    }).then(()=> {
+      //Toast notification to say successful.
+    });
+  };
+
+  return (
+    <>
+      <div className="rounded-lg bg-gray-700/50 text-gray-400 text-sm">
+        <form onSubmit={sendMessage} className="flex flex-grow space-x-5 p-2">
+          <input
+            type="text"
+            placeholder="Type your message here..."
+            className="flex-grow bg-transparent p-2 text-gray-100 placeholder:px-1 focus:outline-none disabled:cursor-not-allowed disabled:text-gray-300"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            autoFocus
+            disabled={!session}
+          />
+          <button
+            type="submit"
+            className="bg-[#11A37F] hover:opacity-50 text-white font-bold px-4 py-2 rounded disabled:bg-gray-300 disabled:cursor-not-allowed "
+            disabled={!prompt || !session}
+          >
+            <PaperAirplaneIcon className="h-4 w-4 -rotate-45"></PaperAirplaneIcon>
+          </button>
+        </form>
+      </div>
+      {/* ModelSelection */}
+      <div className="py-2 md:hidden"></div>
+    </>
+  );
+}
+
+export default ChatInput;
