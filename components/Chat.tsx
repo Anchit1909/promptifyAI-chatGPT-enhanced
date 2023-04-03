4d745de07cf0db93fe791d3f0fa724264fe993db
Anchit Sinha <anchit1909@gmail.com>
2023-04-04 02:08:34 +0530

    Implemented message display in UI

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -1,9 +1,47 @@
+"use client";
+
+import { db } from "@/firebase";
+import { collection, orderBy, query } from "firebase/firestore";
+import { useSession } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import Message from "./Message";
+import { ArrowDownCircleIcon } from "@heroicons/react/24/outline";
+
 type Props = {
   chatId: string;
 };
 
 function Chat({ chatId }: Props) {
-  return <div className="flex-1">Chat</div>;
+  const { data: session } = useSession();
+  const [messages] = useCollection(
+    session &&
+      query(
+        collection(
+          db,
+          "users",
+          session?.user?.email!,
+          "chats",
+          chatId,
+          "messages"
+        ),
+        orderBy("createdAt", "asc")
+      )
+  );
+  return (
+    <div className="flex-1 overflow-y-auto overflow-x-hidden">
+      {messages?.empty && (
+        <>
+          <p className="mt-10 text-center text-white">
+            Type a prompt in below to get started!
+          </p>
+          <ArrowDownCircleIcon className="mx-auto mt-5 h-10 w-10 animate-bounce text-white" />
+        </>
+      )}
+      {messages?.docs.map((message) => (
+        <Message key={message.id} message={message.data()} />
+      ))}
+    </div>
+  );
 }
 
 export default Chat;
